545e6525d61aac115e6bde16bef9024e9c817209
amir ali <AmirAliShar2257@users.noreply.github.com>
2025-07-12 19:32:28 +0500

    minor changes

--- Model/prediction.py
+++ Model/prediction.py
@@ -1,16 +1,13 @@
 import pandas as pd
 import pickle
 
-#load the model
-with open("Model/Diabets_Models.pkl","rb") as f:
-    model =pickle.load(f)
+# Load model dynamically (Vercel sets the working directory to /var/task)
+with open("/var/task/Model/Diabets_Models.pkl", "rb") as f:
+    model = pickle.load(f)
 
-model_version ="1.0.0"
+model_version = "1.0.0"
 
-def Prediction(user_input:dict):
-
-    input_df =pd.DataFrame([user_input])
-
-    output =model.predict(input_df)[0]
-
+def Prediction(user_input: dict):
+    input_df = pd.DataFrame([user_input])
+    output = model.predict(input_df)[0]
     return output
